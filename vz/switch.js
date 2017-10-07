93f722fe8346c0a07a9928b00a2e269b6173841e
vinubaburaj <32588357+vinubaburaj@users.noreply.github.com>
2017-10-07 17:25:49 +0530

    Extensions commit

---
+++ vz/switch.js
@@ -0,0 +1,11 @@
+
+function switch(){
+			
+			if(body.style.backgroundColor=='white')
+				{body.style.backgroundColor='black';
+			}
+			else
+				{body.style.backgroundColor='white';
+			}
+				console.log("Here");
+		}
